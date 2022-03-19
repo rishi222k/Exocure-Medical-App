a34f5fc3f58df05db1d46f3a61a699473703c4a5
Rishi Raghu <rishiraghu22@yahoo.com>
2022-03-19 18:52:14 +0530

    Tweaks

--- Screens/PreHome.js
+++ Screens/PreHome.js
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View,ImageBackground,Image} from 'react-native'
+import { StyleSheet, Text, View,ImageBackground,Image,TouchableOpacity,Button} from 'react-native'
 import React from 'react'
 
 const PreHome = () => {
@@ -7,9 +7,20 @@ const PreHome = () => {
     <ImageBackground source={require('../Images/PreHome-Bg.png')} resizeMode="cover" style={styles.imagebg}>
     <View style={styles.container}>
     <Image source={require('../Images/Logo.png')} resizeMode="contain" style={{width:140,height:140}}/>
-    <Text style={{fontFamily:"SFNS-Bold",fontSize:35,marginVertical:15}}>MedFS</Text>
-    <Text style={{fontFamily:""}}>A medical application to assess early signs of peripheral neuropathy to prevent Diabetic foot syndrome (DFS)</Text>
-    <Text>PreHome</Text>
+    <Text style={{fontFamily:"SFNS-Bold",fontSize:40,marginVertical:15}}>MedFS</Text>
+    <Text style={{fontFamily:"CircularXXTT-Regular",fontSize:20.3,width:"85%",color:'#6F6F6F',lineHeight:27}}>A medical application to assess early signs of peripheral neuropathy to prevent Diabetic foot syndrome (DFS)</Text>
+    </View>
+    <View style={{height:'20%',width:'100%'}}>
+    <TouchableOpacity>
+    <View style={styles.but1}>
+    <Text style={{fontFamily:"CircularXXTT-Bold",color:"white", fontSize:18,textAlign:'center'}}>Create an account</Text>
+    </View>
+    </TouchableOpacity>
+    <TouchableOpacity>
+    <View style={styles.but2}>
+    <Text style={{fontFamily:"CircularXXTT-Bold",color:"#FFB31D", fontSize:18,textAlign:'center',}}>Login</Text>
+    </View>
+    </TouchableOpacity>
     </View>
     </ImageBackground>
     </View>
@@ -23,8 +34,25 @@ const styles = StyleSheet.create({
     height:'100%',
   },
   container:{
-    height:'90%',
-    paddingHorizontal:25,
+    height:'80%',
+    paddingHorizontal:30,
     justifyContent:'center'
+  },
+  but1:{
+    width:"70%",
+    backgroundColor:"#FFB31D",
+    borderRadius:5,
+    marginBottom:12,
+    paddingVertical:12,
+    alignSelf:'center'
+  },
+  but2:{
+    width:"70%",
+    borderRadius:5,
+    marginBottom:12,
+    paddingVertical:12,
+    alignSelf:'center',
+    borderWidth: 3,
+    borderColor:"#FFB31D",
   }
 })
