8c3831272d300d73c20f15a3f793d2fe4f427bba
Rishi Raghu <rishiraghu22@yahoo.com>
2022-03-19 14:02:24 +0530

    Navigation Setup

--- App.js
+++ App.js
@@ -1,21 +1,32 @@
+import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 import { StyleSheet, Text, View } from 'react-native';
+import PreHome from './Screens/PreHome';
+import Login from './Screens/Login';
+import SignUp from './Screens/SignUp';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 export default function App() {
+  const Stack = createStackNavigator();
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <SafeAreaProvider>
+      <Stack.Navigator>
+        <Stack.Screen name='PreHome' component={PreHome} options={{headerShown:false}}/>
+        <Stack.Screen name='Login' component={Login} options={{headerShown:false}}/>
+        <Stack.Screen name='SignUp' component={SignUp} options={{headerShown:false}}/>
+        </Stack.Navigator>
+      </SafeAreaProvider>
+      </NavigationContainer>
+    
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    
   },
 });
---
+++ Screens/PreHome.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const PreHome = () => {
+  return (
+    <View>
+      <Text>PreHome</Text>
+    </View>
+  )
+}
+
+export default PreHome
+
+const styles = StyleSheet.create({})
