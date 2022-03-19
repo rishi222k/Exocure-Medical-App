5af508e5e8bd1ff844b1b0e19bee49f4caf4e552
Rishi Raghu <rishiraghu22@yahoo.com>
2022-03-19 17:34:51 +0530

    Font Tweaks

--- Screens/PreHome.js
+++ Screens/PreHome.js
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View,ImageBackground} from 'react-native'
+import { StyleSheet, Text, View,ImageBackground,Image} from 'react-native'
 import React from 'react'
 
 const PreHome = () => {
@@ -6,6 +6,9 @@ const PreHome = () => {
     <View>
     <ImageBackground source={require('../Images/PreHome-Bg.png')} resizeMode="cover" style={styles.imagebg}>
     <View style={styles.container}>
+    <Image source={require('../Images/Logo.png')} resizeMode="contain" style={{width:140,height:140}}/>
+    <Text style={{fontFamily:"SFNS-Bold",fontSize:35,marginVertical:15}}>MedFS</Text>
+    <Text style={{fontFamily:""}}>A medical application to assess early signs of peripheral neuropathy to prevent Diabetic foot syndrome (DFS)</Text>
     <Text>PreHome</Text>
     </View>
     </ImageBackground>
@@ -20,6 +23,8 @@ const styles = StyleSheet.create({
     height:'100%',
   },
   container:{
+    height:'90%',
     paddingHorizontal:25,
+    justifyContent:'center'
   }
 })
