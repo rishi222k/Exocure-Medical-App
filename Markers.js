ff47c6d20f4eef4b30b5b34fbc121e93c94e2817
Rishi Raghu <rishiraghu22@yahoo.com>
2022-05-30 14:21:06 +0530

    Map Markers

---
+++ Markers.js
@@ -0,0 +1,41 @@
+const Markers=[
+    {
+        key:1,
+        title:'Max Speciality Hospital',
+        address:'1 2, Press Enclave Marg, Saket Institutional Area, Saket, New Delhi, Delhi 110017',
+        phone:1126515050,
+        web:'https://www.maxhealthcare.in/',
+        lat:28.5273069,
+        long:77.2035423,
+    },
+    {
+        key:2,
+        title:'Chikitsa Hospital',
+        address:'CHIKITSA HOSPITAL, Pramod Mahajan Marg, B - 4, Saket',
+        phone:1141664443,
+        web:'https://www.practo.com/delhi/hospital/chikitsa-hospital-saket-2',
+        lat:28.5263227,
+        long:77.2044546,
+    },
+    {
+        key:3,
+        title:'Neelu Angels Hospital',
+        address:'J-293, Saket Rd, Block J, Saket, New Delhi, Delhi 110017, India',
+        phone:911143191300,
+        web:'http://ww1.sawanneeluhospital.com',
+        lat:28.5221424,
+        long:77.213006,
+    },
+    {
+        key:4,
+        title:'Geetanjali Hospital',
+        address:'3, Mmtc, S T C Colony, Opposite Geetangali Enclave, Saket, Saket, New Delhi, Delhi 110017',
+        phone:8003098718,
+        web:'https://www.geetanjalihospital.co.in',
+        lat:28.532864,
+        long:77.204150,
+    },
+
+]
+
+export default Markers;
