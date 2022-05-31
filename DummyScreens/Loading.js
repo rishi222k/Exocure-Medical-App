ac73f162c85350456f8fe7eccd1fc4c2f2613ead
Rishi Raghu <rishiraghu22@yahoo.com>
2022-05-31 17:22:38 +0530

    Loading Map Screen

---
+++ DummyScreens/Loading.js
@@ -0,0 +1,40 @@
+import { StyleSheet, Text, View,ScrollView,Image,TouchableOpacity } from 'react-native'
+import { useNavigation } from '@react-navigation/native';
+import React from 'react'
+import LoadingAnim from '../LoadingAnim'
+
+const Loading = () => {
+    const navigation = useNavigation();
+  return (
+    <ScrollView style={{backgroundColor:"#fff",height:"100%",}}>
+    <View style={{alignItems:"center",marginTop:100}}>
+      <LoadingAnim width={504} height={200} />
+      <Text style={{fontFamily:"CircularXXTTBold", color:"#3A3A3A", fontSize:19, width:"70%",textAlign:"center",marginTop:10}}>
+      Looking for footcare services near you ... </Text>
+      
+    </View>
+    <TouchableOpacity
+      onPress={()=>{navigation.navigate('TabNavigation', { screen: 'Health' })}}>
+        <View style={styles.but2}>
+            <Text style={{fontFamily:"CircularXXTTBold",color:"white", fontSize:17,textAlign:'center'}}>
+            Exit
+            </Text>
+          </View>
+    </TouchableOpacity>
+    </ScrollView>
+  )
+}
+
+export default Loading
+
+const styles = StyleSheet.create({
+    but2:{
+        width:"50%",
+        backgroundColor:"#FFB31D",
+        borderRadius:5,
+        marginBottom:45,
+        paddingVertical:12,
+        alignSelf:"center",
+        marginTop:135
+    },
+})
