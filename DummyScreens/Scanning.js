851e4709763d888b6bc4f2a6bd21a2e1b371d7d7
Rishi Raghu <rishiraghu22@yahoo.com>
2022-05-31 17:23:04 +0530

    Scanning Device Screen

---
+++ DummyScreens/Scanning.js
@@ -0,0 +1,43 @@
+import { StyleSheet, Text, View,ScrollView,Image,TouchableOpacity } from 'react-native'
+import { useNavigation } from '@react-navigation/native';
+import React from 'react'
+import BluetoothAnim from '../BluetoothAnim'
+
+const Scanning = () => {
+
+    const navigation = useNavigation();
+
+  return (
+    <ScrollView style={{backgroundColor:"#fff",height:"100%"}}>
+    <View style={{alignItems:"center",marginTop:10}}>
+      <BluetoothAnim width={330} height={330} />
+      <Text style={{fontFamily:"CircularXXTTBold", color:"#3A3A3A", fontSize:20, width:"90%",textAlign:"center",marginTop:40}}>
+      Looking for devices near you ... </Text>
+      <Text style={{fontFamily:"CircularXXTTMedium", color:"#3A3A3A", fontSize:16, width:"70%",textAlign:"center",marginTop:15}}>
+      Please wait for atleast 60 seconds before retrying </Text>
+    </View>
+    <TouchableOpacity
+      onPress={()=>{navigation.navigate('TabNavigation', { screen: 'Sense' })}}>
+        <View style={styles.but2}>
+            <Text style={{fontFamily:"CircularXXTTBold",color:"white", fontSize:17,textAlign:'center'}}>
+            Cancel
+            </Text>
+          </View>
+    </TouchableOpacity>
+    </ScrollView>
+  )
+}
+
+export default Scanning
+
+const styles = StyleSheet.create({
+    but2:{
+        width:"50%",
+        backgroundColor:"#FFB31D",
+        alignSelf:"center",
+        borderRadius:5,
+        marginBottom:45,
+        paddingVertical:12,
+        marginTop:85
+    },
+})
