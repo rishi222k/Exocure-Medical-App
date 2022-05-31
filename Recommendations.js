8f90ea47a2dbf04085cad1644175e37683e3883f
Rishi Raghu <rishiraghu22@yahoo.com>
2022-05-31 17:22:14 +0530

    Health Recommendations

---
+++ Recommendations.js
@@ -0,0 +1,25 @@
+const Recommendations= [
+    {
+    condition:"Mild",
+    recommendation1:"Every day, wash your feet.",
+    recommendation2:"Use a wound dressing or bandage to clean the wound on a daily basis. Walking barefoot should be avoided. ",
+    recommendation3:"Use a wound dressing or bandage to clean the wound on a daily basis. Walking barefoot should be avoided. ",
+    medication:"Nafcillin, Ampicillin, Cefazolin, Ampicillin"
+    },
+    {
+    condition:"Moderate",
+    recommendation1:"Maintain the health of your feet by keeping them dry and hydrated.",
+    recommendation2:"Constantly use a clean and bandaged ulcer.",
+    recommendation3:"Putting on shoes that are the right size. Getting rid of dead skin and tissue.",
+    medication:"Nafcillin, Ampicillin, Cefazolin, Ampicillin"
+    },
+    {
+    condition:"Severe",
+    recommendation1:"Consulting a podiatrist for removing corn and callus. ",
+    recommendation2:"Getting rid of dead skin and tissue.",
+    recommendation3:"By eliminating foot deformities, a surgeon can help relieve pressure surrounding your ulcer.",
+    medication:"Dicloxacillin, Cephalexin, Clavulanate , Potassium"
+    },
+];
+
+export default Recommendations;
